23f2a848dc0502ca30d1131c9874505d9d65d888
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-02-12 00:19:03 +0530

    directories

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var port = 3000;
 app.use("/", (req, res) => {
   res.sendFile(__dirname + "/Main/login.html");
 });
+app.use("/signup", (req, res) => {
+  res.sendFile(__dirname + "/Main/signup.html");
+});
 app.listen(port, () => {
-  console.log("Server listening on port" + port);
+  console.log("Server listening on port " + port);
 });
