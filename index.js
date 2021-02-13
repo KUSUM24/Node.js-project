06338779d52c60d98b2687f35af7adc2f01b63d5
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-02-13 19:50:56 +0530

    mongoDB connected

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 
+const connectDB = require("./DB/Connection");
+connectDB();
+
 app.set("view engine", "ejs");
 app.use("/assets", express.static("css"));
 
 app.get("/", (req, res) => {
   res.render("login");
-  // res.sendFile(__dirname + "/Main/login.html");
 });
 app.get("/signup", (req, res) => {
   res.render("signup");
